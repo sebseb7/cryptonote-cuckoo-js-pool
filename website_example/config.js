52c59cd113c7582b9d2e689ee66386ee45565eda
sebseb7 <sebgreenbus@gmail.com>
2020-06-17 19:29:40 -0400

    genesis for tube4 stagenet

---
+++ website_example/config.js
@@ -0,0 +1,16 @@
+var api = "https://mining.bittube.app:8123";
+
+var coinDecimalPlaces = 4;
+
+var email = "info@bittube.app";
+var telegram = "https://t.me/bittubeapp";
+var discord = "https://discordapp.com/invite/fBNQRQV";
+
+var marketCurrencies = ["{symbol}-BTC"];
+
+var blockchainExplorer = "http://139.162.211.9:6662/block/{id}";
+var transactionExplorer = "http://139.162.211.9:6662/tx/{id}";
+
+var themeCss = "themes/default.css";
+var themeCssDark = "themes/moon_gray_dark.css";
+var defaultLang = 'en';
